48351da2ab8b3e9a82a1830f569c494f1f2370c9
subhajitpatra498 <subhajitpatra498@gmail.com>
2025-11-13 00:36:47 +0530

    add swagger api in all service

--- Api-Gateway/src/main/java/com/medicare/Api_Gateway/Route/RouteValidator.java
+++ Api-Gateway/src/main/java/com/medicare/Api_Gateway/Route/RouteValidator.java
@@ -21,7 +21,17 @@ public class RouteValidator {
             "/auth/refreshToken",
             "/auth/google-login",
             "/auth/.well-known/jwks.json",
-            "/api/eureka"         // Eureka service registry (should remain open)
+            "/api/eureka" ,        // Eureka service registry (should remain open)
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html"
     );
 
     /**
